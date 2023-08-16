e9de198ab40aa1eae10aa537157c46bade6c9dcf
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2023-08-16 11:40:55 +0200

    testing

--- Testing/MyScene.cs
+++ Testing/MyScene.cs
@@ -14,19 +14,14 @@ public class MyScene: Scene
         var e = new Entity();
         e.AddComponent(new TransformComponent(new Vec2(100)));
         e.AddComponent(new RectComponent(Color.Black, new Vec2(50)));
-        e.AddComponent(new ControlComponent());
+        e.AddComponent(new ControlComponent(speed: 200, useGamePad: true, gamePadIndex: 0));
         e.AddComponent(new CollisionComponent(new Vec2(50)));
         AddEntity(e);
         
         var e2 = new Entity();
         e2.AddComponent(new TransformComponent(new Vec2(300)));
         e2.AddComponent(new RectComponent(Color.Red, new Vec2(50)));
-        e2.AddComponent(new CollisionComponent(new Vec2(50), Vec2.Zero, false, CollisionCallback));
+        e2.AddComponent(new CollisionComponent(new Vec2(50)));
         AddEntity(e2);
-    }
-
-    private void CollisionCallback(Entity from, Entity other)
-    {
-        DebugManager.Log(LogLevel.LogInfo, $"Collision !");
     }
 }
