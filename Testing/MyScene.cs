2c4d4ed3326838edca0b90ae593452d558d6d508
Alexis Huvier <alexis.huvier@alumni.univ-avignon.fr>
2023-09-04 22:08:01 +0200

    update(test): Test drawDebug

--- Testing/MyScene.cs
+++ Testing/MyScene.cs
@@ -15,13 +15,13 @@ public class MyScene: Scene
         e.AddComponent(new TransformComponent(new Vec2(100)));
         e.AddComponent(new RectComponent(Color.Black, new Vec2(50)));
         e.AddComponent(new ControlComponent(speed: 200, useGamePad: true, gamePadIndex: 0));
-        e.AddComponent(new CollisionComponent(new Vec2(50)));
+        e.AddComponent(new CollisionComponent(new Vec2(50), drawDebug: true));
         AddEntity(e);
         
         var e2 = new Entity();
         e2.AddComponent(new TransformComponent(new Vec2(300)));
-        e2.AddComponent(new RectComponent(Color.Red, new Vec2(50)));
-        e2.AddComponent(new CollisionComponent(new Vec2(50)));
+        e2.AddComponent(new RectComponent(Color.Cyan, new Vec2(50)));
+        e2.AddComponent(new CollisionComponent(new Vec2(50), drawDebug: true));
         AddEntity(e2);
     }
 }
