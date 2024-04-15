7248095af93f28ae9766f1939750b705ae084f65
Alexis Huvier <lavapower84@gmail.com>
2024-04-15 16:34:50 +0200

    feat(demo): test du fix interaction

--- Testing/MyScene.cs
+++ Testing/MyScene.cs
@@ -2,8 +2,10 @@ using SharpEngine.Core;
 using SharpEngine.Core.Component;
 using SharpEngine.Core.Entity;
 using SharpEngine.Core.Input;
+using SharpEngine.Core.Manager;
 using SharpEngine.Core.Math;
 using SharpEngine.Core.Utils;
+using SharpEngine.Core.Widget;
 
 namespace Testing;
 
@@ -11,26 +13,28 @@ public class MyScene : Scene
 {
     public MyScene()
     {
-        var e = new Entity();
-        e.AddComponent(new TransformComponent(new Vec2(640, 480)));
-        e.AddComponent(
-            new SpriteSheetComponent(
-                "portal",
-                new Vec2(100),
-                [ new("idle", Enumerable.Range(0, 41).Select(x => (uint)x).ToList(), 0.01f) ],
-                currentAnim: "idle",
-                zLayerOffset: 1
-            )
-        );
-        e.AddComponent(new ControlComponent());
-        AddEntity(e);
+        var button = new Button(
+                       new Vec2(100, 100),
+                                  "Hello World",
+                                             "RAYLIB_DEFAULT",
+                                                        new Vec2(200, 50),
+                                                                   Color.White,
+                                                                              Color.Blue,
+                                                                                         20
+                                                                                                );
+
+        button.Clicked += (_, _) => Console.WriteLine("Button Clicked");
+
+        AddWidget(button);
     }
 
-    public override void OpenScene()
+    public override void Update(float deltaTime)
     {
-        base.OpenScene();
+        base.Update(deltaTime);
 
-        Window!.CameraManager.FollowEntity = Entities[0];
-        Window!.CameraManager.Mode = CameraMode.FollowSmooth;
+        if (InputManager.IsKeyPressed(Key.A))
+        {
+            Widgets[^1].Displayed = false;
+        }
     }
 }
