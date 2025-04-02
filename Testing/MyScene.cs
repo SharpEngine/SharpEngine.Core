7859a488a2b95d3644509e35779d925fb0db724a
Alexis Huvier <lavapower84@gmail.com>
2025-04-02 20:07:21 +0200

    feat(testing): test lineinput modifications

--- Testing/MyScene.cs
+++ Testing/MyScene.cs
@@ -16,43 +16,7 @@ public class MyScene : Scene
 
     public MyScene()
     {
-        var parent = new Entity();
-        parent.AddComponent(new TransformComponent(new Vec2(100, 200)));
-        parent.AddComponent(new SpriteSheetComponent("portal", new Vec2(100), [
-            new("animation", Enumerable.Range(0, 10).Select(x => Convert.ToUInt32(x)).ToList(), 0.2f, true)
-        ], "animation"));
-        AddEntity(parent);
-
-        var movable = new Entity();
-        movable.AddComponent(new TransformComponent(new Vec2(100)));
-        movable.AddComponent(new SpriteSheetComponent("portal", new Vec2(100), [
-            new("animation", Enumerable.Range(0, 10).Select(x => Convert.ToUInt32(x)).ToList(), 0.2f, false)
-        ], "animation"));
-        movable.AddComponent(new ControlComponent());
-        parent.AddChild(movable);
-
-        var entity = new Entity();
-        entity.AddComponent(new TransformComponent(new Vec2(300)));
-        _sprite = entity.AddComponent(new SpriteSheetComponent("portal", new Vec2(100), [
-            new("animation", Enumerable.Range(0, 10).Select(x => Convert.ToUInt32(x)).ToList(), 0.2f, false)
-        ], "animation"));
-        _sprite.AnimationEnded += (_, _) => Console.WriteLine($"Animation Ended : {_sprite.Anim}");
-        _text = entity.AddComponent(new TextComponent("0", offset: new Vec2(0, 50), fontSize: 25));
-        movable.AddChild(entity);
-
-        var button = new Button(
-            new Vec2(100, 100),
-            "Hello World",
-            "RAYLIB_DEFAULT",
-            new Vec2(200, 50),
-            Color.White,
-            Color.Blue,
-            20
-         );
-
-        button.Clicked += (_, _) => Console.WriteLine("Button Clicked");
-
-        AddWidget(button);
+        AddWidget(new LineInput(new Vec2(500), font: "RAYLIB_DEFAULT", fontSize: 25));
     }
 
     public override void Update(float deltaTime)
@@ -61,13 +25,12 @@ public class MyScene : Scene
 
         if (InputManager.IsKeyPressed(Key.A))
         {
-            Widgets[^1].Displayed = false;
-        }
-        if(InputManager.IsKeyPressed(Key.Z))
-        {
-            _sprite.Replay();
+            DebugManager.Log(LogLevel.LogInfo, ((LineInput)Widgets[0]).Text);
         }
 
-        _text.Text = _sprite.CurrentImage.ToString();
+        if(InputManager.IsKeyPressed(Key.Z))
+        {
+            ((LineInput)Widgets[0]).Secret = !((LineInput)Widgets[0]).Secret;
+        }
     }
 }
