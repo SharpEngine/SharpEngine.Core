72757f3cdc74e5f676a3c31fb2c0058a1c4b0fc3
Alexis Huvier <lavapower84@gmail.com>
2025-05-07 20:51:43 +0200

    feat(datatable): rework with add, remove and get functions

--- SharpEngine.Core/Data/DataTable/IDataTable.cs
+++ SharpEngine.Core/Data/DataTable/IDataTable.cs
@@ -1,16 +1,29 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace SharpEngine.Core.Data.DataTable;
 
 /// <summary>
 /// Table of Data
 /// </summary>
-public interface IDataTable
+public interface IDataTable<T> where T : class
 {
+    /// <summary>
+    /// Add Object
+    /// </summary>
+    /// <param name="obj">Object</param>
+    public void Add(T obj);
+
+    /// <summary>
+    /// Remove Object
+    /// </summary>
+    /// <param name="obj">Object</param>
+    public void Remove(T obj);
+
     /// <summary>
     /// Get Object
     /// </summary>
     /// <param name="predicate">Predicate</param>
-    /// <returns>Object</returns>
-    public dynamic? Get(Predicate<dynamic?> predicate);
+    /// <returns>Objects</returns>
+    public IEnumerable<T> Get(Func<T, bool> predicate);
 }
--- SharpEngine.Core/Data/DataTable/JsonDataTable.cs
+++ SharpEngine.Core/Data/DataTable/JsonDataTable.cs
@@ -10,17 +10,30 @@ namespace SharpEngine.Core.Data.DataTable;
 /// Json Data Table
 /// </summary>
 /// <param name="jsonFile">Json File</param>
-public class JsonDataTable<T>(string jsonFile) : IDataTable
+public class JsonDataTable<T>(string jsonFile) : IDataTable<T> where T : class
 {
-    private List<dynamic?> Objects { get; } =
-            JsonSerializer
-                .Deserialize<List<T>>(File.ReadAllText(jsonFile))
-                ?.Select(x => (dynamic?)x)
-                .ToList() ?? [];
+    private List<T> Objects { get; } = JsonSerializer.Deserialize<List<T>>(File.ReadAllText(jsonFile))?.ToList() ?? [];
 
-    /// <inheritdoc />
-    public dynamic? Get(Predicate<dynamic?> predicate)
+    /// <inheritdoc/>
+    public void Add(T obj)
     {
-        return Objects.Find(predicate);
+        ArgumentNullException.ThrowIfNull(obj);
+        Objects.Add(obj);
+        File.WriteAllText(jsonFile, JsonSerializer.Serialize(Objects));
+    }
+
+    /// <inheritdoc/>
+    public IEnumerable<T> Get(Func<T, bool> predicate)
+    {
+        ArgumentNullException.ThrowIfNull(predicate);
+        return Objects.Where(obj => predicate(obj));
+    }
+
+    /// <inheritdoc/>
+    public void Remove(T obj)
+    {
+        ArgumentNullException.ThrowIfNull(obj);
+        Objects.Remove(obj);
+        File.WriteAllText(jsonFile, JsonSerializer.Serialize(Objects));
     }
 }
